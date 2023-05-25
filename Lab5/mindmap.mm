0b34fda2c93d4f5c2b784af82a9498da047fe591
IrmA-ErroR <81929851+IrmA-ErroR@users.noreply.github.com>
2023-05-25 15:13:23 +0300

    Add mindmap

---
+++ Lab5/mindmap.mm
@@ -0,0 +1,217 @@
+<map version="freeplane 1.11.1">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="ГОСТ Р 51898-2002 АСПЕКТЫ БЕЗОПАСНОСТИ Правила включения в стандарты" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1685010347086">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="0.269">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false" associatedTemplateLocation="template:/light_nord_template.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="5. Концепция безопасности" POSITION="bottom_or_right" ID="ID_1835903794" CREATED="1685010642074" MODIFIED="1685014044531">
+<node TEXT="Не может быть абсолютной безопасности — некоторый риск, определенный в настоящем стандарте как остаточный, будет оставаться." ID="ID_941236108" CREATED="1685013724062" MODIFIED="1685013941257"/>
+<node TEXT="Безопасность достигают путем снижения уровня риска до допустимого...Допустимый риск представляет собой оптимальный баланс между безопасностью и требованиями, которым должны удовлетворять продукция, процесс или услуга, а также такими факторам выгодность для пользователя, эффективность затрат, обычаи и др." ID="ID_178180051" CREATED="1685013726263" MODIFIED="1685014015913"/>
+<node TEXT="5.3 Допустимый риск достигают с помощью итеративного процесса оценки и уменьшения риска" ID="ID_279222153" CREATED="1685013727128" MODIFIED="1685014667493">
+<hook URI="file:///C:/Users/sveta/Pictures/Screenshots/Снимок%20экрана%20(36).png" SIZE="0.7237636" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="6. Достижение допустимого риска" POSITION="bottom_or_right" ID="ID_1018062139" CREATED="1685010642541" MODIFIED="1685014163808">
+<node TEXT="Для достижения уровня риска до допустимого применяют следующую процедуру" ID="ID_1817165690" CREATED="1685014235820" MODIFIED="1685014704564">
+<arrowlink DESTINATION="ID_279222153"/>
+</node>
+<node TEXT="Способы уменьшения риска следующие (в порядке приоритетов):&#xa;- разработка безопасного в своей основе проекта;&#xa;- защитные устройства и персональное защитное оборудование;&#xa;- информация по установке и применению;&#xa;- обучение;" ID="ID_1603026931" CREATED="1685014498504" MODIFIED="1685014765023">
+<node TEXT="Уменьшение рисков" ID="ID_289641323" CREATED="1685014767467" MODIFIED="1685014779751">
+<hook URI="file:///C:/Users/sveta/Pictures/Screenshots/Снимок%20экрана%20(37).png" SIZE="0.83798885" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+<node TEXT=" Включение аспектов безопасности в стандарты" POSITION="bottom_or_right" ID="ID_1344264860" CREATED="1685010643363" MODIFIED="1685010722851">
+<node TEXT="Типы стандартов на безопасность" ID="ID_674248632" CREATED="1685010841043" MODIFIED="1685014840840">
+<node TEXT="- основополагающие стандарты" ID="ID_1751052599" CREATED="1685014844149" MODIFIED="1685014980330">
+<node TEXT="включающие в себя фундаментальные концепции, принципы и требования, относящиеся к основным аспектам безопасности. Эти стандарты применяют для широкого диапазона видов продукции, процессов и услуг" ID="ID_687721147" CREATED="1685014903390" MODIFIED="1685014980330" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="- групповые стандарты" ID="ID_679513887" CREATED="1685014851854" MODIFIED="1685014871700">
+<node TEXT="включающие в себя аспекты безопасности, применимые&#xa;к нескольким видам или к семейству близких видов продукции, процессов или услуг. В этих стандартах,&#xa;насколько возможно, делают ссылки на основополагающие стандарты на безопасность" ID="ID_1261480352" CREATED="1685014908957" MODIFIED="1685014967391" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="- стандарты на безопасность продукции" ID="ID_744307297" CREATED="1685014852246" MODIFIED="1685014882433">
+<node TEXT=" включающие в себя аспекты безопасности определенного вида или семейства продукции, процессов или услуг. В этих стандартах, насколько возможно, делают&#xa;ссылки на основополагающие стандарты на безопасность и групповые стандарты на безопасность" ID="ID_1684946577" CREATED="1685014946257" MODIFIED="1685014967392" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="- стандарты на продукцию" ID="ID_967965832" CREATED="1685014853351" MODIFIED="1685014902655">
+<node TEXT=" содержащие аспекты безопасности, но касающиеся не только аспектов безопасности. В них должны быть сделаны ссылки на основополагающие стандарты на безопасность и групповые стандарты на безопасность" ID="ID_827145081" CREATED="1685014956361" MODIFIED="1685014967393" BACKGROUND_COLOR="#009999"/>
+</node>
+</node>
+<node TEXT="Анализ предложений по разработке (пересмотру) стандартов" ID="ID_1561565415" CREATED="1685015017740" MODIFIED="1685015089687">
+<node TEXT="а) Кому адресован стандарт?" ID="ID_1149219547" CREATED="1685015069613" MODIFIED="1685015085443">
+<node TEXT="- Кто будет (намерен) использовать стандарт.&#xa;- Каким образом будет использован стандарт.&#xa;- Что требуют от стандарта пользователи, включая тех, кто:&#xa;- обеспечивает выполнение требований стандарта;&#xa;- испытывает на себе его воздействие;&#xa;- испытывает на себе воздействие окружающей среды." ID="ID_1071771108" CREATED="1685015123695" MODIFIED="1685015177153" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="б) Каким должен быть тип стандарта?" ID="ID_121040666" CREATED="1685015071232" MODIFIED="1685015093861" VSHIFT_QUANTITY="-0.6 pt">
+<node TEXT="Должен ли это быть:&#xa;- основополагающий стандарт на безопасность;&#xa;- групповой стандарт на безопасность;&#xa;- стандарт на безопасность продукции;&#xa;- стандарт на продукцию, содержащий аспекты безопасности" ID="ID_633681715" CREATED="1685015143517" MODIFIED="1685015177155" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="в) В чем состоит основная цель стандарта?" ID="ID_749176082" CREATED="1685015072178" MODIFIED="1685015108305">
+<node TEXT="- Какие возникают аспекты, относящиеся к безопасности.&#xa;- Будет ли стандарт использован для испытаний.&#xa;- Будет ли стандарт служить основой для подтверждения соответствия." ID="ID_952396152" CREATED="1685015155267" MODIFIED="1685015177155" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="г) Как должен быть написан стандарт?" ID="ID_643364023" CREATED="1685015072927" MODIFIED="1685015122738">
+<node TEXT="- Какую подготовку или знания предположительно могут или должны иметь пользователи&#xa;стандарта." ID="ID_1179569678" CREATED="1685015166109" MODIFIED="1685015177156" BACKGROUND_COLOR="#009999"/>
+</node>
+</node>
+<node TEXT="Подготовительная работа" ID="ID_203332818" CREATED="1685015186980" MODIFIED="1685015195278">
+<node TEXT="- определение аспектов безопасности, которые должны быть охвачены стандартом:&#xa;- сбор необходимой информации;&#xa;- подготовка плана будущего стандарта;&#xa;- подготовка заключения специалистов, в котором должны быть отражены сведения, требующиеся для разработки стандарта, в том числе:&#xa;- сведения о продукции, процессе или услуге:&#xa;- сведения о несчастных случаях/инцидентах;&#xa;- информация от потребителей на основе опыта использования продукции, процесса или услуги;&#xa;- сведения о доступных защитных мерах;&#xa;- сведения о перспективных разработках или образцах продукции, процесса или услуги,&#xa;- правовые ограничения." ID="ID_1539149249" CREATED="1685015195280" MODIFIED="1685015374135">
+<node TEXT="- предназначенное использование;&#xa;- возможное предсказуемое неправильное использование;&#xa;- способность к действию при ожидаемых условиях использования;&#xa;- совместимость с окружающей средой;&#xa;- эргономические факторы;&#xa;- безотказность;&#xa;- ремонтопригодность и удобство обслуживания;&#xa;- долговечность;&#xa;- возможность утилизации (включая любые необходимые инструкции);&#xa;- специальные потребности пользователей продукции, процесса или услуги, например детей, пожилых людей, групп людей с ограниченными возможностями;&#xa;- характеристики отказов;&#xa;- маркировка и информация;&#xa;- существующие стандарты типов, перечисленных" ID="ID_110960947" CREATED="1685015214850" MODIFIED="1685015374134" BACKGROUND_COLOR="#009999"/>
+</node>
+</node>
+<node TEXT="Разработка проектов стандартов" ID="ID_790844582" CREATED="1685015257285" MODIFIED="1685015261387">
+<node TEXT="Общие положения" ID="ID_485951395" CREATED="1685015273675" MODIFIED="1685015285627"/>
+<node TEXT=" Информация для безопасного пользования" ID="ID_991398435" CREATED="1685015275327" MODIFIED="1685015310006">
+<node TEXT=" Виды информации" ID="ID_1360789802" CREATED="1685015347320" MODIFIED="1685015385093" BACKGROUND_COLOR="#009999"/>
+<node TEXT="Инструкции для пользования" ID="ID_878346643" CREATED="1685015352812" MODIFIED="1685015385093" BACKGROUND_COLOR="#009999"/>
+<node TEXT="Предупреждающие надписи" ID="ID_1523265506" CREATED="1685015353315" MODIFIED="1685015385089" BACKGROUND_COLOR="#009999"/>
+</node>
+<node TEXT="Упаковка" ID="ID_237103182" CREATED="1685015311349" MODIFIED="1685015314749"/>
+<node TEXT="Безопасность при испытаниях" ID="ID_444863127" CREATED="1685015316092" MODIFIED="1685015331830"/>
+</node>
+</node>
+<node TEXT="1. Область применения" POSITION="top_or_left" ID="ID_529641385" CREATED="1685010646887" MODIFIED="1685013279050">
+<node TEXT="Настоящий стандарт устанавливает для разработчиков стандартов правила включения в стандарты аспектов безопасности. Стандарт может быть применен к любым аспектам безопасности, относящимся к людям или имуществу, или окружающей среде, или к сочетанию этих сторон." ID="ID_675415416" CREATED="1685011106016" MODIFIED="1685011136400"/>
+<node TEXT="Правила, устанавливаемые настоящим стандартом, основаны на уменьшении риска, возникающего при использовании продукции, процессов или услуг." ID="ID_527015352" CREATED="1685011143256" MODIFIED="1685011156894"/>
+<node TEXT="Стандарт рассматривает полный жизненный цикл продукции, процесса или услуги, включая как&#xa;предназначенное использование, так и возможное предсказуемое неправильное использование." ID="ID_1879812076" CREATED="1685011158860" MODIFIED="1685011168515"/>
+</node>
+<node TEXT="2. Нормативные ссылки" POSITION="top_or_left" ID="ID_728573792" CREATED="1685010647348" MODIFIED="1685013282395">
+<node TEXT="ГОСТ Р 12.4.026—2001 Система стандартов безопасности труда. Цвета сигнальные, знаки&#xa;безопасности и разметка сигнальная. Назначение и правила применения. Общие технические&#xa;требования и характеристики. Методы испытаний." ID="ID_1740858392" CREATED="1685011190152" MODIFIED="1685011201351"/>
+</node>
+<node TEXT="3. Определения" POSITION="top_or_left" ID="ID_625026630" CREATED="1685010647794" MODIFIED="1685013287504">
+<font SIZE="16" BOLD="true"/>
+<node TEXT="3.1 безопасность" ID="ID_1606516227" CREATED="1685011228294" MODIFIED="1685012932027">
+<font SIZE="16" BOLD="true"/>
+<node TEXT="Отсутствие недопустимого риска" ID="ID_1373481951" CREATED="1685012941264" MODIFIED="1685012945829"/>
+</node>
+<node TEXT="3.2 риск" ID="ID_1998934827" CREATED="1685011270080" MODIFIED="1685012953817">
+<font BOLD="true"/>
+<node TEXT="Сочетание вероятности нанесения ущерба и тяжести этого ущерба" ID="ID_361661073" CREATED="1685012960053" MODIFIED="1685012963133"/>
+</node>
+<node TEXT="3.3 ущерб" ID="ID_13503825" CREATED="1685011270510" MODIFIED="1685012972341">
+<font BOLD="true"/>
+<node TEXT="Нанесение физического повреждения или другого вреда здоровью людей, или вреда&#xa;имуществу или окружающей среде" ID="ID_1653933438" CREATED="1685012979675" MODIFIED="1685012983071"/>
+</node>
+<node TEXT="3.4 вызывающее ущерб событие" ID="ID_1547685075" CREATED="1685011270837" MODIFIED="1685012989881">
+<font BOLD="true"/>
+<node TEXT="Событие, при котором опасная ситуация приводит к ущербу" ID="ID_1624474639" CREATED="1685012990533" MODIFIED="1685012992534"/>
+</node>
+<node TEXT="3.5 опасность" ID="ID_383614629" CREATED="1685011271195" MODIFIED="1685013019825">
+<font BOLD="true"/>
+<node TEXT="Потенциальный источник возникновения ущерба." ID="ID_1982986922" CREATED="1685013006396" MODIFIED="1685013008131">
+<node TEXT="Примечание — Термин «опасность» может быть конкретизирован в части определения природы опасности или вида ожидаемого ущерба (например, опасность электрического шока, опасность разрушения, травматическая опасность, токсическая опасность, опасность пожара, опасность утонуть)." ID="ID_336089166" CREATED="1685013022298" MODIFIED="1685013201795" BACKGROUND_COLOR="#009999"/>
+</node>
+</node>
+<node TEXT="3.6 опасная ситуация" ID="ID_241013353" CREATED="1685011271546" MODIFIED="1685013040298">
+<font BOLD="true"/>
+<node TEXT="Обстоятельства, в которых люди, имущество или окружающая среда подвергаются опасности" ID="ID_1053819056" CREATED="1685013041258" MODIFIED="1685013042731"/>
+</node>
+<node TEXT="3.7 допустимый риск" ID="ID_1861532314" CREATED="1685011271904" MODIFIED="1685013048191">
+<font BOLD="true"/>
+<node TEXT="Риск, который в данной ситуации считают приемлемым при существующих общественных ценностях" ID="ID_217757815" CREATED="1685013049293" MODIFIED="1685013050609"/>
+</node>
+<node TEXT="3.8 защитная мера" ID="ID_970722711" CREATED="1685011272292" MODIFIED="1685013057348">
+<font BOLD="true"/>
+<node TEXT="Мера, используемая для уменьшения риска" ID="ID_834108045" CREATED="1685013057997" MODIFIED="1685013059663"/>
+</node>
+<node TEXT="3.9 остаточный риск" ID="ID_1962814224" CREATED="1685011272688" MODIFIED="1685013065854">
+<font BOLD="true"/>
+<node TEXT="Риск, остающийся после предпринятых защитных мер" ID="ID_1059494991" CREATED="1685013066598" MODIFIED="1685013074413"/>
+</node>
+<node TEXT="3.10 анализ риска" ID="ID_44623350" CREATED="1685013497139" MODIFIED="1685013540089">
+<font BOLD="true"/>
+<node TEXT="Систематическое использование информации для выявления опасности и&#xa;количественной оценки риска" ID="ID_1171730889" CREATED="1685013531922" MODIFIED="1685013534767"/>
+</node>
+<node TEXT="3.11 оценивание риска" ID="ID_1908000835" CREATED="1685011273067" MODIFIED="1685013540093">
+<font BOLD="true"/>
+<node TEXT="Основанная на результатах анализа риска процедура проверки, устанавливающая, не превышен ли допустимый риск" ID="ID_268097349" CREATED="1685013519872" MODIFIED="1685013523887"/>
+</node>
+<node TEXT="3.12 оценка риска" ID="ID_1969075506" CREATED="1685011273487" MODIFIED="1685013540093">
+<font BOLD="true"/>
+<node TEXT="Общий процесс анализа риска и оценивания риска" ID="ID_654189031" CREATED="1685013417746" MODIFIED="1685013424211"/>
+</node>
+<node TEXT="3.13 предназначенное использование" ID="ID_1399960917" CREATED="1685013329376" MODIFIED="1685013540094">
+<font BOLD="true"/>
+<node TEXT="Использование продукции, процесса или услуги в соответствии с информацией, представленной поставщиком" ID="ID_450782350" CREATED="1685013335118" MODIFIED="1685013393961"/>
+</node>
+<node TEXT="3.14 возможное предсказуемое неправильное использование" ID="ID_1112180266" CREATED="1685013339812" MODIFIED="1685013540094">
+<font BOLD="true"/>
+<node TEXT="Использование продукции,&#xa;процесса или услуги не предназначенным поставщиком образом, а вследствие предсказуемого поведения человека" ID="ID_1939325411" CREATED="1685013356594" MODIFIED="1685013371943"/>
+</node>
+</node>
+<node TEXT="4. Использование слов «безопасность» и «безопасный»" POSITION="top_or_left" ID="ID_57855843" CREATED="1685010893478" MODIFIED="1685013564180">
+<node TEXT="...только для выражения уверенности и&#xa;гарантий риска... Рекомендуется всюду,&#xa;где возможно, эти слова заменять признаками предмета, например:&#xa;- «защитный шлем» вместо «безопасный шлем»;&#xa;- «нескользское покрытие для пола» вместо «безопасное покрытие»." ID="ID_446994643" CREATED="1685013566451" MODIFIED="1685013664584"/>
+</node>
+</node>
+</map>
